fb0baf5c59ef4ff83c099aa2d29737d9903497a6
rociobuono <91210086+rociobuono@users.noreply.github.com>
2024-04-18 13:51:16 -0300

    Comente el nav.mjs y agregue un hover efect

--- app/Componentes/Nav.mjs
+++ app/Componentes/Nav.mjs
@@ -12,11 +12,10 @@ const Nav = () => {
 
         return $a;
     }
-    const crearImg = (id, clase, src) => {
+    const crearImg = (id, src) => {
         let $img = document.createElement("img");
         $img.setAttribute("id", id);
         $img.src = src;
-        $img.class = clase;
         return $img;
     }
     const crearLista = ($a) => {
@@ -26,14 +25,22 @@ const Nav = () => {
         $ul.appendChild($li);
         return $ul;
     }
-    $idNav.appendChild(crearImg("logoNavbar", "logo", "./resources/Logo.png"));
-    $idNav.appendChild(crearLista(crearA("idInicio", "#sectInicio", "INICIO")));
+    $idNav.appendChild(crearImg("logoNavbar", "./resources/Logo.png")); //crear el logo y meterlo en el nav
+    $idNav.appendChild(crearLista(crearA("idInicio", "#sectInicio", "INICIO"))); //crear los botones y meterlos en el nav
     $idNav.appendChild(crearLista(crearA("idCartelera", "#sectCartelera", "CARTELERA")));
     $idNav.appendChild(crearLista(crearA("idCombos", "#sectCombos", "COMBOS")));
 
-    const $inicio = document.getElementById("idInicio");
+    const $inicio = document.getElementById("idInicio"); //Estos son los botones
     const $cartelera = document.getElementById("idCartelera");
     const $combos = document.getElementById("idCombos");
+
+    //HOVER EFECT 
+    $inicio.addEventListener('mouseover', () =>{$inicio.style.color ="#CFDEDF"});
+    $inicio.addEventListener('mouseout', () =>{$inicio.style.color ="#fff"});
+    $cartelera.addEventListener('mouseover', () =>{$cartelera.style.color ="#CFDEDF"});
+    $cartelera.addEventListener('mouseout', () =>{$cartelera.style.color ="#fff"});
+    $combos.addEventListener('mouseover', () =>{$combos.style.color ="#CFDEDF"});
+    $combos.addEventListener('mouseout', () =>{$combos.style.color ="#fff"});
 
     const crearH = (num, id, txt) => {
         let $h = document.createElement(`h${num}`);
@@ -41,11 +48,12 @@ const Nav = () => {
         $h.setAttribute("id", id);
         return $h;
     }
-    $sectInicio.appendChild(crearH(1, "tituloInicio", "ESTE ES EL INICIO"));
+    $sectInicio.appendChild(crearH(1, "tituloInicio", "ESTE ES EL INICIO")); //Titulos de las secciones
     $sectCombos.appendChild(crearH(1, "tituloCombos", "ESTA ES LA SECCION COMBOS"));
     $sectCartelera.appendChild(crearH(1, "tituloCartelera", "ESTA ES LA CARTELERA"));
-    
-    $inicio.addEventListener("click", () => { 
+
+    //Funciones para que al hacer click en los botones del nav solo aparezcan las secciones pertinentes
+    $inicio.addEventListener("click", () => {  
         $sectInicio.style.display = "block"; 
         $sectCombos.style.display = "none";
         $sectCartelera.style.display = "none";
